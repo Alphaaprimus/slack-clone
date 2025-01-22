94037c078b344784a99ceedb6b7867924801dc07
Yattharth Arnav Singh <yattharth@Yattharths-MacBook-Pro.local>
2025-01-22 23:25:48 +0530

    Adding Channels

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -8,10 +8,13 @@ import { useGetChannels } from "@/features/channels/api/use-get-channels";
 import { WorkspaceSection } from "./workspace-section";
 import { useGetMembers } from "@/features/members/api/use-get-members";
 import { UserItem } from "./user-item";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
 
 export const WorkspaceSidebar = () => {
 
     const workspaceId = useWorkspaceId();
+
+    const [_open, setOpen] = useCreateChannelModal();  
 
     const {data: member, isLoading: memberLoading} = useCurrentMember({ workspaceId });
     const {data: workspace, isLoading: workspaceLoading} = useGetWorkspace({ id: workspaceId });
@@ -54,10 +57,10 @@ export const WorkspaceSidebar = () => {
                 id="drafts"
                 />
                 </div>
-                <WorkspaceSection 
+                <WorkspaceSection  
                 label="Channels"
                 hint="New Channel"
-                onNew={() => {}}
+                onNew={member.role === "admin" ? () => setOpen(true) : undefined}
                 >
                 {channels?.map((item) => (
                     <SidebarItem
