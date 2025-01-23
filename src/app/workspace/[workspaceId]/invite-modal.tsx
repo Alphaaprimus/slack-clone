36e321362ab16a69fdccae890d91631e2ceec858
Yattharth Arnav Singh <yattharth@Yattharths-MacBook-Pro.local>
2025-01-23 17:03:43 +0530

    New code generation

--- src/app/workspace/[workspaceId]/invite-modal.tsx
+++ src/app/workspace/[workspaceId]/invite-modal.tsx
@@ -7,8 +7,11 @@ import {
     DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
+import { useNewJoinCode } from "@/features/workspaces/api/use-new-join-code";
+import { useConfirm } from "@/hooks/use-confirm";
 import { useWorkspaceId } from "@/hooks/use-workspaceid";
-import { CopyIcon } from "lucide-react";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { CopyIcon, RefreshCcw } from "lucide-react";
 import { toast } from "sonner";
   
 interface InviteModalProps {
@@ -26,6 +29,31 @@ export const InviteModal = ({
 }: InviteModalProps) => {
 
     const workspaceId = useWorkspaceId();
+
+    const [ConfirmDialog, confirm] = useConfirm(
+        "Are you sure?",
+        "This will deactivate the current invite code and generate a new one."
+    );
+
+    const { mutate, isPending } = useNewJoinCode();
+     
+    const handleNewCode = async () => {
+
+        const ok = await confirm();
+
+        if (!ok) return;
+
+        mutate ({workspaceId}, {
+            onSuccess: () => {
+                toast.success("Invite code regenreated")
+            },
+            onError: () => {
+                toast.error("Failed to generate invite code")
+            }
+
+        })
+    }
+
     const handleCopy = () => {
         
         const inviteLink = `${window.location.origin}/join/${workspaceId}`
@@ -36,6 +64,8 @@ export const InviteModal = ({
     };
 
     return (
+        <>
+        <ConfirmDialog/>
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogContent>
                 <DialogHeader>
@@ -59,7 +89,18 @@ export const InviteModal = ({
                         <CopyIcon className="size-4 ml-2"/>
                     </Button>
                 </div>
+                <div className="flex items-center justify-between w-full">
+                    <Button disabled={isPending} onClick={handleNewCode} variant="outline">
+                        New Code
+                        <RefreshCcw className="size-4 ml-2"/>
+                    </Button>
+                    <DialogClose asChild>
+                        <Button>Close</Button>
+                    </DialogClose>
+
+                </div>
             </DialogContent>
         </Dialog>
+        </>
     )
 }
