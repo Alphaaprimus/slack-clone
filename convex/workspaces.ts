346cbb04ac1991a7d09010dd05fde4ec962e835c
Yattharth Arnav Singh <yattharth@Yattharths-MacBook-Pro.local>
2025-01-24 15:42:16 +0530

    Invite feature added

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -14,6 +14,52 @@ const generateCode = () => {
 
 };
 
+export const join = mutation ({
+    args: {
+        joinCode: v.string(),
+        workspaceId: v.id("workspaces")
+    },
+    handler: async (ctx,args) => {
+        const userId = await getAuthUserId(ctx)
+
+        if (!userId) {
+            throw new Error("Unauthorised");
+        }
+
+        const workspace = await ctx.db.get(args.workspaceId);
+
+        if (!workspace) {
+            throw new Error("Workspace not found")
+        }
+
+        if (workspace.joinCode !== args.joinCode.toLowerCase() ){
+            throw new Error("Invalid join code");
+        }
+
+        const existingMember = await ctx.db
+        .query("members")
+        .withIndex("by_workspace_id_user_id", (q) => 
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+    )
+    
+    .unique();
+
+    if (existingMember) {
+        throw new Error("Already a member of this workspace");
+    }
+
+    await ctx.db.insert("members", {
+        userId,
+        workspaceId: workspace._id,  
+        role: "member"
+    });
+    
+    return workspace._id;
+
+    } 
+
+})
+
 export const newJoinCode = mutation({
     args: {
         workspaceId: v.id("workspaces"),
@@ -117,6 +163,32 @@ export const get = query({
 
     },
 }); 
+
+export const getInfoById = query({
+
+    args: { id: v.id("workspaces") },
+    handler: async (ctx, args) => {
+         const userId  = await auth.getUserId(ctx);
+
+         if (!userId) {
+             return null;
+         }
+
+        const member = await ctx.db
+        .query("members")
+        .withIndex("by_workspace_id_user_id", (q) => 
+            q.eq("workspaceId", args.id).eq("userId",userId),
+        )
+        .unique();
+
+        const workspace = await ctx.db.get(args.id);
+        
+        return {
+            name: workspace?.name,
+            isMember: !!member
+        };
+    },
+})
 
 export const getById = query({
     args: { id: v.id("workspaces") },
