001c507a15a9beb51bb1f033c3bbe27e1f9e75f4
Yattharth Arnav Singh <yattharth@Yattharths-MacBook-Pro.local>
2025-01-27 14:44:14 +0530

    header component done

--- src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
@@ -13,6 +13,14 @@ import { TrashIcon } from "lucide-react";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { DialogClose } from "@radix-ui/react-dialog";
+import { useUpdateChannel } from "@/features/channels/api/use-update-channel";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { toast } from "sonner";
+import { useRemoveChannel } from "@/features/channels/api/use-remove-channel";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/hooks/use-workspaceid";
+import { useCurrentMember } from "@/features/members/api/use-current-member";
 
 interface HeaderProps {
     title: string;
@@ -20,17 +28,72 @@ interface HeaderProps {
 
 export const Header = ({ title }: HeaderProps) => {
 
+    const router = useRouter(); 
+
+    const channelId= useChannelId();
+
+    const workspaceId = useWorkspaceId();   
+
+    const [ ConfirmDialog, confirm ] = useConfirm(
+        "Delete this channel",
+        "You are about to delete this channel this action is irreversible.",
+    );
+
     const [ editOpen, setEditOpen ] = useState(false);
     const [ value, setValue ] = useState(title); // [setValue]
+
+    const { data: member } =useCurrentMember({ workspaceId });    
+
+    const { mutate: updateChannel, isPending: isUpdatingChannel } = useUpdateChannel();
+    const { mutate: removeChannel, isPending: isRemovingChannel } = useRemoveChannel();
+
+    const handleEditOpen = ( value: boolean ) => {
+        if (member?.role !== "admin") return;  
+
+        setEditOpen(value);
+
+    };
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
         setValue(value);
       }
 
+    const handleDelete = async () => {
+        const ok = await confirm();
+
+        if (!ok) return;
+
+        removeChannel({ id: channelId }, {
+            onSuccess: () => {
+                toast.success("Channel deleted");
+                router.push(`/workspace/${workspaceId}`)
+            },
+
+            onError: () => {
+                toast.error("Failed to delete channel")
+            }
+        });
+    }
+
+      const handleSubmit =  (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        updateChannel({ id: channelId, name: value}, {
+            onSuccess: () => {
+                toast.success("Channel updated");
+                setEditOpen(false);
+            },
+            onError: () => {
+                toast.error("Failed to update channel")
+            }
+        })
+      }
+
 
     return (
         <div className="bg-white border-b h-[49px] flex items-center px-4 overflow-hidden">
+            <ConfirmDialog/>
             <Dialog>
                 <DialogTrigger asChild>
             <Button
@@ -51,14 +114,16 @@ export const Header = ({ title }: HeaderProps) => {
                     </DialogTitle>
                 </DialogHeader>
                 <div className="px-4 pb-4 flex flex-col gap-y-2">
-                    <Dialog open={editOpen} onOpenChange={setEditOpen}>
+                    <Dialog open={editOpen} onOpenChange={handleEditOpen}>
                         <DialogTrigger asChild>
                     <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
                         <div className="flex items-center justify-between">
                             <p className="text-sm font-semibold">Channel Name</p>
+                            {member?.role === "admin" && (
                             <p className="text-sm text-[#1264a3] hover:underline font-semibold">
                                 Edit
                             </p>
+                            )}
                         </div>
                         <p className="text-sm"># {title}</p>
                     </div> 
@@ -68,10 +133,10 @@ export const Header = ({ title }: HeaderProps) => {
                             <DialogTitle>
                                 Rename this title
                             </DialogTitle>
-                            <form className="space-y-4">
+                            <form onSubmit={handleSubmit} className="space-y-4">
                                 <Input
                                  value={value}
-                                 disabled={false}
+                                 disabled={isUpdatingChannel}
                                  onChange={handleChange}
                                  required
                                  autoFocus
@@ -81,11 +146,11 @@ export const Header = ({ title }: HeaderProps) => {
                                 />
                                 <DialogFooter>
                                     <DialogClose asChild>
-                                        <Button variant="outline" disabled={false}>
+                                        <Button variant="outline" disabled={isUpdatingChannel}>
                                             Cancel
                                         </Button>
                                     </DialogClose>
-                                    <Button disabled={false}>
+                                    <Button disabled={isUpdatingChannel}>
                                         Save
                                     </Button>
                                 </DialogFooter>
@@ -95,6 +160,8 @@ export const Header = ({ title }: HeaderProps) => {
                     </DialogContent>
                     </Dialog>
                     <button
+                        onClick={handleDelete}
+                        disabled={isRemovingChannel}
                         className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg cursor-pointer border hover:bg-gray-50 text-rose-600"
                     >
                         <TrashIcon className="size-4"/>
