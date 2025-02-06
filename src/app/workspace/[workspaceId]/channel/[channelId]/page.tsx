8224e489146da14b9ffc5c8fdc21a30745ba2fec
Yattharth Arnav Singh <yattharth@Yattharths-MacBook-Pro.local>
2025-02-06 16:03:02 +0530

    messages

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -7,16 +7,17 @@ import { Header } from "./header";
 import { ChatInput } from "./chat-input";
 import { useGetMesssages } from "@/features/messages/api/use-get-messages";
 import { resourceLimits } from "worker_threads";
+import { MessageList } from "@/components/message-list";
 
 const ChannelIdPage = () => {
 
     const channelId = useChannelId();
 
-    const { results } = useGetMesssages({ channelId });
+    const { results, status, loadMore } = useGetMesssages({ channelId });
 
     const  { data: channel, isLoading: channelLoading } = useGetChannel({ id: channelId});
 
-    if (channelLoading) {
+    if (channelLoading || status === "LoadingFirstPage") {
         return (
             <div className="h-full flex-1 flex items-center justify-center">
                 <Loader className="animate-spin size-5 text-muted-foreground"/>
@@ -38,9 +39,14 @@ const ChannelIdPage = () => {
     return (
         <div className="flex flex-col h-full">
             <Header title={channel.name}/>
-            <div className="flex-1" >
-                {JSON.stringify(results)}
-            </div>
+            <MessageList
+            channelName={channel.name}
+            channelCreationTime={channel._creationTime}
+            data={results}
+            loadMore={loadMore}
+            isLoadingMore={status === "LoadingMore"}  
+            canLoadMore={status === "CanLoadMore"}    
+            />
             <ChatInput placeholder={`Message #${channel.name}`}/>
         </div>
     );
